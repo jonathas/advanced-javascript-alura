0921f397ab75a535ec651ae61efb6b4a38774620
Jon Ribeiro <contact@jonathas.com>
2018-12-23 11:48:22 +0100

    Implement ajax call using the XMLHttpRequest object

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -26,6 +26,28 @@ class NegociacaoController {
         this._limpaFormulario();
     }
 
+    importaNegociacoes() {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    JSON.parse(xhr.responseText).map(objeto =>
+                        this._listaNegociacoes.adiciona(
+                            new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)
+                        )
+                    );
+                    this._mensagem.texto = 'Negociações importadas com sucesso';
+                } else {
+                    console.log('Não foi possível obter as negociações do servidor');
+                    this._mensagem.texto = 'Não foi possível obter as negociações da semana';
+                }
+            }
+        };
+        xhr.send();
+    }
+
     apaga() {
 
         this._listaNegociacoes.esvazia();
